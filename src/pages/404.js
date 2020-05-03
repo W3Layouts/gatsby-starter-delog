2a7ffcbb3caf3eaf1f6397c2dc2e5c92dc19c68a
Munwr <hi@munwr.com>
2020-05-04 02:42:06 +0530

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -37,7 +37,7 @@ const notFound = () => {
             fill="currentColor"
           />
         </svg>
-        <p>Either you are in a wrong page or you have lost. Let go back <Link to="/">home</Link> safely</p>
+        <p>Either you are in a wrong page or you have lost. Lets go back <Link to="/">home</Link> safely</p>
       </div>
     </Layout>
   )
