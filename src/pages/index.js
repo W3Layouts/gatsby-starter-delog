caa162fef52ce2b2a6f26ea19c22adea0b1707c3
w3hidayath <51093439+w3hidayath@users.noreply.github.com>
2020-05-11 22:27:32 +0530

    Removed contact form domain verification

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,6 @@ const IndexPage = ({
       <Helmet>
         <title>{site.siteMetadata.title}</title>
         <meta name="description" content={site.siteMetadata.description} />
-        {!site.siteMetadata.w3l_dom_key ? null : <meta name="w3l-domain-verification" content={site.siteMetadata.w3l_dom_key} />}
       </Helmet>
       <HeroHeader/>
       <h2>Blog Posts &darr;</h2>
